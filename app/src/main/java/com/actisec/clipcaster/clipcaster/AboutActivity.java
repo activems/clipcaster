43c80440d0f3a66e5b344173d867b2c42cee58ff
Xiao Bao Clark <x.clark@actisec.com>
2014-10-28 14:20:15 +1300

    Started service in AboutActivity

--- app/src/main/java/com/actisec/clipcaster/clipcaster/AboutActivity.java
+++ app/src/main/java/com/actisec/clipcaster/clipcaster/AboutActivity.java
@@ -58,6 +58,7 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(FirstRunDialog.getView(this));
+        startService(new Intent(this, ClipCasterService.class));
     }
 
 }
